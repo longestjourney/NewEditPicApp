30750dbfd8a37ddf4deaf8aa92000062addde9dd
goldfi2h <natwisnie@wp.pl>
2022-12-13 13:30:29 +0100

    PictureInvert

--- NewPicEditApp/Program.cs
+++ NewPicEditApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,5 +19,6 @@ namespace NewPicEditApp
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PicForm());
         }
+
     }
 }
