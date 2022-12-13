95fdcc945a97795b979821cdd9a9da8dd7e9b2f2
goldfi2h <natwisnie@wp.pl>
2022-12-13 17:54:24 +0100

    a
    a

--- NewPicEditApp/Picture/PictureCreator.cs
+++ NewPicEditApp/Picture/PictureCreator.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace NewPicEditApp
 {
-    internal class PictureCreator
+    abstract internal class PictureCreator
     {
+        Dictionary<string, PictureMake> pictures = new Dictionary<string, PictureMake>();
+        
+
+        private static interface PictureMake
+        {
+             Picture Make(Bitmap bitmap);
+        }
+        public static Picture Make(Dictionary<string, PictureMake> pictures, Bitmap bitmap)
+        {
+            return pictures.V.Make(bitmap);
+        }
     }
+}
 }
